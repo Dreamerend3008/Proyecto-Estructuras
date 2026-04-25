97d4ddf4156b7dc435434f31c6cdcdbe460c42a6
Dreamerend3008 <79518618+Dreamerend3008@users.noreply.github.com>
2026-04-25 09:05:05 -0500

    Cambios leonard

--- QuadTree.h
+++ QuadTree.h
@@ -4,15 +4,13 @@
 #include "Rectangulo.h"
 #include <vector>
 class QuadTree{
-    public:
+    private:
         Rectangulo* espacio;
     public:
         QuadTree(std::vector<Elemento> elementos,int capacidad);
         QuadTree();
         std::vector<Elemento> en_cuadrante(double x1, double x2, double y1, double y2, Rectangulo* rec);//recursiva
         std::vector<Elemento> en_cuadrante(double x1, double x2, double y1, double y2);
-
-
 };
 
 #endif
